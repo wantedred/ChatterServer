ea68d93880424e59e3133308ebda31c462c22954
Tristan <wantedrednight@gmail.com>
2020-05-25 17:51:15 -0500

    Add project files.

---
+++ WebChatterServer/Hubs/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebChatterServer.Hubs
+{
+    public class Message
+    {
+        public string ClientId { get; set; }
+
+        public string Type { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
---
+++ WebChatterServer/Hubs/MessageHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace WebChatterServer.Hubs
+{
+    public class MessageHub : Hub
+    {
+        public async Task NewMessage(Message msg)
+        {
+            await Clients.All.SendAsync("MessageReceived", msg);
+        }
+    }
+}
