6ff9cc65cea320c24beef52a32ccac7bc44e43de
wantedred <wantedrednight@gmail.com>
2020-08-10 02:51:46 -0500

    Git test

--- WebChatterServer/Startup.cs
+++ WebChatterServer/Startup.cs
@@ -30,7 +30,7 @@ namespace WebChatterServer
             Configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container. 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
